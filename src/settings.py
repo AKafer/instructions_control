a205bd6b03ad74995b38e7ca976bf4be52288659
Sergei Storozhuk <s_storozhuk@wargaming.com>
2024-07-24 17:44:06 +0300

    fix env varibles names

--- src/settings.py
+++ src/settings.py
@@ -9,9 +9,9 @@ DATABASE_POOL_SIZE = os.getenv("DB_POOL_SIZE", default=20)
 DATABASE_POOL_MAX_OVERFLOW = os.getenv("DB_POOL_MAX_OVERFLOW", default=5)
 DATABASE_HOST = os.getenv("DB_HOST", default="localhost")
 DATABASE_PORT = os.getenv("DB_PORT", default=5432)
-DATABASE_NAME = os.getenv("DB_NAME", default="")
-DATABASE_USER = os.getenv("DB_USER", default="")
-DATABASE_PASSWORD = os.getenv("DB_PASSWORD", default="")
+DATABASE_NAME = os.getenv("POSTGRES_DB", default="")
+DATABASE_USER = os.getenv("POSTGRES_USER", default="")
+DATABASE_PASSWORD = os.getenv("POSTGRES_PASSWORD", default="")
 DATABASE_URL = (
     f"postgresql+asyncpg://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 )
