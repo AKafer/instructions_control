f3a10fbfa502a9bcd50a37a2edb7764cc4a6b292
Sergei Storozhuk <s_storozhuk@wargaming.com>
2024-08-07 12:26:26 +0300

    add CORS

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi_pagination import add_pagination
+from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import PlainTextResponse
 from starlette.staticfiles import StaticFiles
 
@@ -11,6 +12,9 @@ def setup_routes(app: FastAPI):
     app.add_route("/ping/", lambda _request: PlainTextResponse("pong"))
 
 
+origins = ['*']
+
+
 def create_app() -> FastAPI:
     app = FastAPI(
         debug=True,
@@ -19,4 +23,11 @@ def create_app() -> FastAPI:
     setup_routes(app)
     add_pagination(app)
     app.mount("/static", StaticFiles(directory="static"), name="static")
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     return app
