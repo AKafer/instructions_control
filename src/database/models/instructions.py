294c5905c506e0b30ebf420101f0d2c899e986c5
Sergei Storozhuk <s_storozhuk@wargaming.com>
2024-08-19 21:11:22 +0300

    fix length

--- src/database/models/instructions.py
+++ src/database/models/instructions.py
@@ -6,8 +6,8 @@ class Instructions(BaseModel):
     __tablename__ = 'instructions'
 
     id = sa.Column(sa.BigInteger, primary_key=True)
-    title = sa.Column(sa.String(64), nullable=False, unique=True)
-    number = sa.Column(sa.String(64), nullable=True)
-    filename = sa.Column(sa.String(64), nullable=True)
+    title = sa.Column(sa.String(640), nullable=False, unique=True)
+    number = sa.Column(sa.String(320), nullable=True)
+    filename = sa.Column(sa.String(640), nullable=True)
     iteration = sa.Column(sa.Boolean(), nullable=False, default=False)
     period = sa.Column(sa.INTEGER, nullable=True)
