d00139c860402ce03e70c36d0bcc05b0108aaf96
Sergei Storozhuk <s_storozhuk@wargaming.net>
2024-09-01 15:05:47 +0300

    add link

--- src/web/journals/routers.py
+++ src/web/journals/routers.py
@@ -12,7 +12,7 @@ from database.models import User
 from database.models.journals import Journals
 from dependencies import get_db_session
 from main_schemas import ResponseErrorBody
-from web.journals.services import add_params_to_jornals, save_file
+from web.journals.services import add_params_to_jornals, save_file, get_full_link
 from web.journals.shemas import Journal
 from web.users.users import current_user, current_superuser
 
@@ -40,10 +40,14 @@ router = APIRouter(prefix='/journals', tags=['journals'])
     dependencies=[Depends(current_superuser)],
 )
 async def get_all_journals(
+    request: Request,
     db_session: AsyncSession = Depends(get_db_session),
 ):
     query = select(Journals).order_by(Journals.id.desc())
     response = await paginate(db_session, query)
+    for journal in response.items:
+        if journal.signature is not None:
+            journal.signature = get_full_link(request, journal.signature)
     return response
 
 
