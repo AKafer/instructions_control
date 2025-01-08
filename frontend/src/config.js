56815439efd395251b25d02205e7698e8f41abf4
Sergei Storozhuk <s_storozhuk@wargaming.net>
2025-01-08 16:27:27 +0200

    renew nginx configuration

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,2 @@
-export const apiBaseUrl = window._env_?.REACT_APP_API_BASE_URL || 'http://localhost:8500';
+// export const apiBaseUrl = window._env_?.REACT_APP_API_BASE_URL || 'http://localhost:8500';
+export const apiBaseUrl = '';
