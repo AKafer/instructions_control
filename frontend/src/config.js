0b533393f052a2d7b4e0071d0c67fea9ac352a38
Sergei Storozhuk <s_storozhuk@wargaming.net>
2025-01-19 11:53:18 +0200

    fix work url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,10 @@
 // production
-export const apiBaseUrl = window._env_?.REACT_APP_API_BASE_URL || 'http://localhost:8500';
+export const apiBaseUrl = '';
 
 // dev
 // export const apiBaseUrl = 'http://0.0.0.0:8500';
+
+// old flow
+// export const apiBaseUrl = window._env_?.REACT_APP_API_BASE_URL || 'http://localhost:8500';
 
 
